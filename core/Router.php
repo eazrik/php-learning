e40179d958e57de06024594f06dba70450e7a5c2
eazrik <eazrik.zauli@dekatku.com>
2018-02-05 23:26:32 +0800

    refactor code

---
+++ core/Router.php
@@ -0,0 +1,33 @@
+<?php
+
+class Router {
+
+    protected $routes = [];
+
+    public function define($routes)
+    {
+
+        $this->routes = $routes;
+
+    }
+
+    public function direct($uri)
+    {
+        if (array_key_exists($uri, $this->routes)) {
+            return $this->routes[$uri];
+        }
+
+        throw new Exception("Error Processing Request", 1);
+
+    }
+
+    public static function load($file)
+    {
+
+        $router = new static;
+
+        require $file;
+
+        return $router;
+    }
+}
--- views/index.view.php
+++ views/index.view.php
@@ -12,6 +12,12 @@
     </style>
 </head>
 <body>
+    <nav>
+        <ul>
+            <li><a href="/about.php">About</a></li>
+            <li><a href="/contact.php">Contact</a></li>
+        </ul>
+    </nav>
 
     <ul>
         <?php foreach ($tasks as $task) : ?>
