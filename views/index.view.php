7c2e469d5114ccb86818b781a6c3963f6f2534c4
eazrik <eazrik.zauli@dekatku.com>
2018-02-11 20:44:58 +0800

    now can add data to db

--- views/index.view.php
+++ views/index.view.php
@@ -1,6 +1,12 @@
 
 <?php require 'partials/header.php'; ?>
 
+<ul>
+<?php foreach ($users as $user) : ?>
+    <li><?= $user->name; ?></li>
+<?php endforeach; ?>
+</ul>
+
 <form method="POST" action="/names">
 
     <input type="text" name="name"></input>
